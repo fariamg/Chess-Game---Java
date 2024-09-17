0fe6a9049ea3176c2c91127f4bc7ee0ef8d51097
Gabriel <fariamg4@gmail.com>
2024-09-17 00:03:37 -0300

    Special move - Promotion (repair)

--- src/main/java/chessLayer/entities/ChessMatch.java
+++ src/main/java/chessLayer/entities/ChessMatch.java
@@ -6,8 +6,6 @@ import boardLayer.entities.Position;
 import chessLayer.entities.pieces.*;
 import chessLayer.enums.Color;
 import chessLayer.exceptions.ChessException;
-
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -117,7 +115,7 @@ public class ChessMatch  {
         if (promoted == null)
             throw new IllegalStateException("There is no piece to be promoted");
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q"))
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted
 
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
